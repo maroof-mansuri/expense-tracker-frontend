7bab17613267173ec5dd815e01778dce76c75cd9
Maroof_Mansuri <117035966+maroof-mansuri@users.noreply.github.com>
2026-01-16 00:16:37 +0530

    Update proxy to use Vercel deployment URL

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,9 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://127.0.0.1:5000",
+        target: "https://expense-tracker-backend-nine-lovat.vercel.app",
         changeOrigin: true,
-        secure: false,
+        secure: true,
       },
     },
   },
